ac95397bc7ac863c3b19e054f64b3ad6a6a457a7
Vinicius Machuca <vmachuca@salesforce.com>
2023-02-08 14:34:54 -0300

    added customer address fields mapping

---
+++ sfcc/cartridges/plugin_cdp_export_jobsteps/cartridge/scripts/map/CustomerMap.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.addressFields = [
+    "UUID",
+    "ID",
+    "customerNo",
+    "address1",
+    "address2",
+    "city",
+    "stateCode",
+    "countryCode",
+    "postalCode",
+    "firstName",
+    "lastName",
+    "fullName",
+    "companyName",
+    "creationDate",
+    "lastModified"
+];
--- sfcc/cartridges/plugin_cdp_export_jobsteps/cartridge/scripts/steps/CustomersToCsv.js
+++ sfcc/cartridges/plugin_cdp_export_jobsteps/cartridge/scripts/steps/CustomersToCsv.js
@@ -3,9 +3,12 @@
 const Status = require('dw/system/Status');
 const Logger = require('dw/system/Logger');
 const CustomerMgr = require('dw/customer/CustomerMgr');
+const CustomerMap = require('../map/CustomerMap');
 const Delta = require('../util/Delta');
 const CsvType = require('../file/CsvType');
 const CsvFile = require('../file/CsvFile');
+
+const FIELD_CUSTOMER_NO = 'customerNo';
 
 function execute(params, stepExecution) {
     try {
@@ -20,15 +23,28 @@ function execute(params, stepExecution) {
 }
 
 function createOutputFile(params) {
-   var csvCustomer = new CsvFile(CsvType.CUSTOMER, params);
-   var csvCustomerAddress = new CsvFile(CsvType.CUSTOMER_ADDRESS, params);
+    var csvCustomer = new CsvFile(CsvType.CUSTOMER, params);
+    var csvCustomerAddress = new CsvFile(CsvType.CUSTOMER_ADDRESS, params, true);
+
+    csvCustomerAddress.addRowFromList(CustomerMap.addressFields);
 
     var qol = Delta.systemObjectQuery(CsvType.CUSTOMER.object, params);
     while(qol.hasNext()) {
+        // profile
         var profile = qol.next();
         csvCustomer.addRow(profile);
+
+        //address
         profile.getAddressBook().addresses.toArray().forEach(address => {
-            csvCustomerAddress.addRow(address);
+            var row = [];
+            CustomerMap.addressFields.forEach(field => {
+                if(field == FIELD_CUSTOMER_NO) {
+                    row.push(profile[FIELD_CUSTOMER_NO]);
+                    return;
+                }
+                row.push(address[field]);
+            });
+            csvCustomerAddress.addRowFromList(row);
         });
     };
     csvCustomer.close();
